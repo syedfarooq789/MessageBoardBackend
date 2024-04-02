a742794fa8bb95e9fa20e793c41ca8f8f81da969
syedfarooq789 <farooqhassan92@hotmail.com>
2024-04-02 17:25:21 +0300

    added graphql module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ChannelInitializationService } from './channel.init.service';
+import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CommonModule } from './common';
+import { DatabaseModule } from './database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+    CommonModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [ChannelInitializationService],
 })
